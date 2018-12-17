67e81422445fcebc393c3579acd040b8aefabe19
Ange <s4nixd@gmail.com>
2018-12-17 09:42:05 +0100

    Starting the api (requests handling)

---
+++ api/README.md
@@ -0,0 +1,5 @@
+# API
+
+## Introduction
+
+This part is such simple, it's receive some request with valids IDs and write the SMS with the number provides.
---
+++ api/index.php
@@ -0,0 +1,56 @@
+<?php 
+    // This is the main of the api systeme for my API SMS
+
+    // Access Control
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+    // Cache control
+    header("Cache-Control: no-store, no-cache, must-revalidate"); 
+    header("Cache-Control: post-check=0, pre-check=0", false); 
+    header("Cache-Control: private");
+    header("Pragma: no-cache");
+    header('Content-Type:application/json; Charset=UTF-8');
+
+    $result = array();
+    $valid_message = false;
+    $valid_number = false;
+    if(isset($_REQUEST['key'])){
+
+        if(isset($_REQUEST['message'])){
+            if(sizeof($_REQUEST['message']) < 100 && sizeof($_REQUEST['message']) > 3){
+                $valid_message = true;
+            }else{
+                $result['status'] = 'error';
+                $result['reason'] = 'Error with the message size (<100 and >3)';
+            }
+        }else{
+            $result['status'] = 'error';
+            $result['reason'] = 'message parameter not found.';
+        }
+
+        if(isset($_REQUEST['number'])){
+            if(sizeof($_REQUEST['number']) > 8 && sizeof($_REQUEST['number']) < 15){
+                $valid_number = true;
+            }else{
+                $result['status'] = 'error';
+                $result['reason'] = 'Error with the number size (<15 and >8)';
+            }
+        }else{
+            $result['status'] = 'error';
+            $result['reason'] = 'number parameter not found.';
+        }
+
+        if($valid_message && $valid_number){
+
+        }
+
+    }else{
+        $result['status'] = 'error';
+        $result['reason'] = 'key not found.';
+    }
+
+    echo json_encode($result);
+?>
